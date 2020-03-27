dec806838303956d72d5953a47e696f753f63976
Matthew Piskunov <mpiskunov@rihousing.com>
2020-03-26 22:14:02 -0400

    ordered effects

--- Controllers/EffectsController.cs
+++ Controllers/EffectsController.cs
@@ -25,7 +25,7 @@ namespace WitcherTRPG_API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Effect>>> GetEffects()
         {
-            return await _context.Effects.ToListAsync();
+            return await _context.Effects.OrderBy(o => o.Name).ToListAsync();
         }
 
         // GET: api/Effects/5
