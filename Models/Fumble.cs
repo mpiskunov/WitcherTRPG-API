2baff4cf2b682a523742e8023ee6958ebba10286
Matthew Piskunov <matthewpiskunov@yahoo.com>
2020-04-04 22:36:17 -0400

    added fumble table

---
+++ Models/Fumble.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WitcherTRPG_API.Models
+{
+    public class Fumble
+    {
+        public int ID { get; set; }
+        public string RollNumber { get; set; }
+        public string Description { get; set; }
+        public int RollType { get; set; }
+    }
+}
